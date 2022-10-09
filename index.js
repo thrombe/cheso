b9a07e4ae7f6371142c4f47b6b64557560b0abd0
thrombe <yash.n.thombare620@gmail.com>
2022-10-10 02:32:15 +0530

    update wasm

---
+++ index.js
@@ -0,0 +1,29 @@
+import init from "/runner.js"
+
+async function load(wasmPath) {
+    const response = await fetch(wasmPath);
+    let receivedLength = 0;
+    let chunks = new Array();
+    const reader = response.body.getReader();
+
+    while (true) {
+        const { done, value } = await reader.read();
+            if (done) {
+                break;
+            } else {
+                receivedLength += value.byteLength;
+                chunks.push(value);
+            }
+    }
+    let wasmBytes = new Uint8Array(receivedLength);
+    let pos = 0;
+    for (let chunk of chunks) {
+        wasmBytes.set(chunk, pos);
+    }
+
+    await init(wasmBytes);
+}
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+load('/runner.wasm');
